5fc552e3b9d6f6fd55c1dd3b78c93994764b99a0
hslam <791874158@qq.com>
2019-10-09 19:55:35 +0800

    add less

--- sortedlist.go
+++ sortedlist.go
@@ -4,6 +4,19 @@ type Less func(i Score,j Score)bool
 type Score interface{}
 type Value interface{}
 
+var LessInt= func(i Score,j Score)bool{
+	if i.(int)<j.(int){return true} else {return false}
+}
+
+var LessInt64= func(i Score,j Score)bool{
+	if i.(int64)<j.(int64){return true} else {return false}
+}
+
+var LessString= func(i Score,j Score)bool{
+	if i.(string)<j.(string){return true} else {return false}
+}
+
+
 type Node struct {
 	score		Score
 	value		Value
